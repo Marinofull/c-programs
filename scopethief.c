843cedf7ebf2995a14c0a363ca49453ea8187ac5
Marinofull <Marinofull@users.noreply.github.com>
2017-06-08 14:26:03 -0300

    improve it

--- scopethief.c
+++ scopethief.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM 50
-#define MUN 3
-
 void thief(){
-    //code here
-    int b = MUN, *c;
+    int *c = NULL, b = 9;
+    // 'c' comes above 'b' in the inverted stack cuz it is a long type, which has priority than int type
 
-    for(c = &b; *c != NUM; c++){
-        printf("c points to %ld storing %d\n", (long)c, *c);
-    }
-    *c = b;
+    //printf("c are in %ld storing %ld\n", (long)&c, 0);
+    //printf("b are in %ld storing %ld\n", (long)&b, b);
+    //for(c = &b; b > 0; b--, c++)
+    //    printf("c points to %ld storing %ld\n", (long)c, *c);
+    //printf("c points to %ld storing %ld\n", (long)c, *c);
+    c = &b + 2*sizeof(int) + 1;
+    //printf("%d\n", *c);
+    *c = *(c+1);
+    //printf("%d\n", *c);
 }
-//Make the print prints MUN instead of NUM
 
 int main() {
-    int a = NUM;
+    int a,b;
+    //'b' comes above 'a' in the inverted stack, if they are the same type
 
+    scanf("%ld %ld", &a, &b);
+    //printf("a are in %ld storing %d\n", (long)&a, a);
+    //printf("b are in %ld storing %d\n", (long)&b, b);
     thief();
-    printf("%d\n", a);
+    printf("a now stores %ld\n", a);
 
     return 0;
 }
